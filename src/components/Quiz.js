a11c90fb9620624919b257a527f1edc696f1dd46
joyjsmun <joy.js.mun@gmail.com>
2022-04-02 23:12:12 +0900

    added last styling

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,12 @@
 export default function Quiz() {
     return(
         <div className="quizMain">
+            
             <div className="question__section">
-            <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <div className="question__menu">
+                <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <p>☰</p>
+            </div>
                 <div className="answer__box">
                     <div className="answer">1</div>
                     <div className="answer">2</div>
@@ -11,6 +15,49 @@ export default function Quiz() {
                 </div>
             <hr/>
             </div>
+
+            <div className="question__section">
+                <div className="question__menu">
+                <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <p>☰</p>
+            </div>
+                <div className="answer__box">
+                    <div className="answer">1</div>
+                    <div className="answer">2</div>
+                    <div className="answer">3</div>
+                    <div className="answer">4</div>
+                </div>
+            <hr/>
+            </div>
+
+            <div className="question__section">
+                <div className="question__menu">
+                <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <p>☰</p>
+            </div>
+                <div className="answer__box">
+                    <div className="answer">1</div>
+                    <div className="answer">2</div>
+                    <div className="answer">3</div>
+                    <div className="answer">4</div>
+                </div>
+            <hr/>
+            </div>
+
+            <div className="question__section">
+                <div className="question__menu">
+                <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <p>☰</p>
+            </div>
+                <div className="answer__box">
+                    <div className="answer">1</div>
+                    <div className="answer">2</div>
+                    <div className="answer">3</div>
+                    <div className="answer">4</div>
+                </div>
+            <hr/>
+            </div>
+
         </div>
     )
 }
