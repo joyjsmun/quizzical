2e17419559614bf94d3213434f640182c3126642
joyjsmun <joy.js.mun@gmail.com>
2022-04-02 22:50:38 +0900

    added styling for question section

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,16 @@
 export default function Quiz() {
     return(
-        <div className="quizMain">Quiz</div>
+        <div className="quizMain">
+            <div className="question__section">
+            <h3 className="question">In what year was the game "Fallout" released?</h3>
+                <div className="answer__box">
+                    <div className="answer">1</div>
+                    <div className="answer">2</div>
+                    <div className="answer">3</div>
+                    <div className="answer">4</div>
+                </div>
+            <hr/>
+            </div>
+        </div>
     )
 }
