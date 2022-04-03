b14649c249eb1ae888b18fdc12e232d5ec805b34
joyjsmun <joy.js.mun@gmail.com>
2022-04-03 23:45:56 +0900

    data fetcher

---
+++ src/components/DataFetcher.js
@@ -0,0 +1,5 @@
+export default function DataFetcher(){
+    return(
+        <div>Fetcher</div>
+    )
+}
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,3 +1,5 @@
+import DataFetcher from "./DataFetcher";
+
 export default function Quiz() {
     return(
         <div className="quizMain">
@@ -15,48 +17,7 @@ export default function Quiz() {
                 </div>
             <hr/>
             </div>
-
-            <div className="question__section">
-                <div className="question__menu">
-                <h3 className="question">In what year was the game "Fallout" released?</h3>
-                <p>☰</p>
-            </div>
-                <div className="answer__box">
-                    <div className="answer">1</div>
-                    <div className="answer">2</div>
-                    <div className="answer">3</div>
-                    <div className="answer">4</div>
-                </div>
-            <hr/>
-            </div>
-
-            <div className="question__section">
-                <div className="question__menu">
-                <h3 className="question">In what year was the game "Fallout" released?</h3>
-                <p>☰</p>
-            </div>
-                <div className="answer__box">
-                    <div className="answer">1</div>
-                    <div className="answer">2</div>
-                    <div className="answer">3</div>
-                    <div className="answer">4</div>
-                </div>
-            <hr/>
-            </div>
-
-            <div className="question__section">
-                <div className="question__menu">
-                <h3 className="question">In what year was the game "Fallout" released?</h3>
-                <p>☰</p>
-            </div>
-                <div className="answer__box">
-                    <div className="answer">1</div>
-                    <div className="answer">2</div>
-                    <div className="answer">3</div>
-                    <div className="answer">4</div>
-                </div>
-            <hr/>
-            </div>
+<DataFetcher />
 
         </div>
     )
